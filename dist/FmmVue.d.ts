b208a511012c794e0f85d0fda0336684c0ee0967
Sparrowhawk <sparrowhawk-ea@hotmail.com>
2021-04-06 16:12:28 +1000

    upload dist to repo

---
+++ dist/FmmVue.d.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import { FmmFramework, FmmMapErrors, FmmMapStore, FmmMapString, FmmMapValues, FmmStore, FmmWidgetFactory } from '@fmmp/core';
+export declare const FmmVueMinimap: import("vue/types/vue").ExtendedVue<Vue, unknown, {
+    destructor(): void;
+    takeSnapshot(): boolean;
+}, unknown, {
+    aggregateLabels: FmmMapString;
+    anchor: HTMLElement;
+    customWidgetIds: string[];
+    debounceMsec: number;
+    dynamicLabels: string[];
+    framework: FmmFramework;
+    page: HTMLElement;
+    panel: Vue;
+    store: FmmStore;
+    title: string;
+    usePanelDetail: boolean;
+    useWidthToScale: boolean;
+    verbosity: number;
+    widgetFactories: FmmWidgetFactory[];
+}>;
+export declare const FmmVuePanel: import("vue/types/vue").ExtendedVue<Vue, unknown, {
+    destroyDetached(): void;
+}, unknown, {
+    detailParent: HTMLDivElement;
+    vertical: boolean;
+}>;
+export declare const FmmVueStore: import("vue/types/vue").ExtendedVue<Vue, {
+    store: FmmMapStore<FmmMapValues, FmmMapErrors>;
+}, unknown, unknown, {
+    errors: FmmMapErrors;
+    values: FmmMapValues;
+}>;
+export declare const FmmVuex: import("vue/types/vue").ExtendedVue<Vue, {
+    $store: any;
+    unsubscribeToStore: () => void;
+    store: FmmMapStore<FmmMapValues, FmmMapErrors>;
+}, unknown, unknown, {
+    errors: FmmMapErrors;
+}>;
---
+++ dist/FmmVue.js
@@ -0,0 +1,231 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FmmVuex = exports.FmmVueStore = exports.FmmVuePanel = exports.FmmVueMinimap = void 0;
+var tslib_1 = require("tslib");
+var vue_1 = tslib_1.__importDefault(require("vue"));
+var core_1 = require("@fmmp/core");
+// =================================================================================================================================
+//						F M M V U E M I N I M A P
+// =================================================================================================================================
+exports.FmmVueMinimap = vue_1.default.extend({
+    // =============================================================================================================================
+    destroyed: function () {
+        var minimap = G.MINIMAPS.get(this);
+        if (minimap)
+            minimap.detach();
+        G.MINIMAPS.delete(this);
+    },
+    // =============================================================================================================================
+    methods: {
+        destructor: function () {
+            var _a;
+            return (_a = G.MINIMAPS.get(this)) === null || _a === void 0 ? void 0 : _a.destructor();
+        },
+        takeSnapshot: function () {
+            var _a;
+            return (_a = G.MINIMAPS.get(this)) === null || _a === void 0 ? void 0 : _a.takeSnapshot();
+        }
+    },
+    // =============================================================================================================================
+    mounted: function () {
+        var _this = this;
+        var _a, _b;
+        if (Object.keys(this.$scopedSlots).length)
+            throw new Error('FmmVueMinimap is a contentless tag');
+        var form = (_a = this.$el) === null || _a === void 0 ? void 0 : _a.parentElement;
+        while (form && form.tagName !== 'FORM')
+            form = form.parentElement;
+        if (!form)
+            throw new Error('FmmVueMinimap must be used inside a FORM tag');
+        var fmcp = {
+            aggregateLabels: this.aggregateLabels,
+            anchor: this.anchor,
+            debounceMsec: this.debounceMsec,
+            dynamicLabels: this.dynamicLabels,
+            form: form,
+            framework: this.framework,
+            onUpdate: function (snapshot) { return _this.$emit('update', snapshot); },
+            page: this.page,
+            store: this.store,
+            title: this.title,
+            usePanelDetail: this.usePanelDetail,
+            useWidthToScale: this.useWidthToScale,
+            verbosity: this.verbosity,
+            widgetFactories: this.widgetFactories
+        };
+        var minimap = (_b = G.PANELS.get(this.panel)) === null || _b === void 0 ? void 0 : _b.createMinimap(fmcp);
+        if (!minimap)
+            return;
+        G.MINIMAPS.set(this, minimap);
+        minimap.compose(this.customWidgetIds);
+        this.$watch('customWidgetIds', function (ids) { return minimap.compose(ids); });
+    },
+    // =============================================================================================================================
+    name: 'FmmVueMinimap',
+    // =============================================================================================================================
+    props: {
+        aggregateLabels: Object,
+        anchor: HTMLElement,
+        customWidgetIds: Array,
+        debounceMsec: Number,
+        dynamicLabels: Array,
+        framework: Object,
+        page: HTMLElement,
+        panel: {
+            required: true,
+            type: Object,
+            validator: function (value) { return value.$options.name === 'FmmVuePanel'; }
+        },
+        store: Object,
+        title: {
+            required: true,
+            type: String,
+            validator: function (value) { return (value === null || value === void 0 ? void 0 : value.trim().length) > 0; }
+        },
+        usePanelDetail: Boolean,
+        useWidthToScale: Boolean,
+        verbosity: Number,
+        widgetFactories: Array
+    },
+    // =============================================================================================================================
+    render: function (ce) { return ce('div'); },
+    // =============================================================================================================================
+    updated: function () {
+        var _this = this;
+        this.$nextTick(function () { var _a; return (_a = G.MINIMAPS.get(_this)) === null || _a === void 0 ? void 0 : _a.takeSnapshot(); });
+    }
+});
+// =================================================================================================================================
+//						F M M V U E P A N E L
+// =================================================================================================================================
+exports.FmmVuePanel = vue_1.default.extend({
+    // =============================================================================================================================
+    destroyed: function () {
+        var panel = G.PANELS.get(this);
+        if (panel)
+            panel.destructor();
+        G.PANELS.delete(this);
+    },
+    // =============================================================================================================================
+    methods: {
+        destroyDetached: function () {
+            var panel = G.PANELS.get(this);
+            if (panel)
+                panel.destroyDetached();
+        }
+    },
+    // =============================================================================================================================
+    mounted: function () {
+        if (Object.keys(this.$scopedSlots).length)
+            throw new Error('FmmVuePanel is a contentless tag');
+        G.PANELS.set(this, core_1.Fmm.createPanel(undefined, this.$el, this.detailParent, this.vertical));
+    },
+    // =============================================================================================================================
+    name: 'FmmVuePanel',
+    // =============================================================================================================================
+    props: {
+        detailParent: {
+            required: true,
+            type: HTMLDivElement
+        },
+        vertical: Boolean
+    },
+    // =============================================================================================================================
+    render: function (ce) {
+        return ce('div', { staticClass: 'fmm-panel' }, [
+            ce('style', {
+                tag: 'component',
+                attrs: { type: 'text/css' }
+            }, core_1.Fmm.CSS)
+        ]);
+    }
+});
+// =================================================================================================================================
+//						F M M V U E S T O R E
+// =================================================================================================================================
+exports.FmmVueStore = vue_1.default.extend({
+    // =============================================================================================================================
+    data: function () {
+        return {
+            store: undefined
+        };
+    },
+    // =============================================================================================================================
+    mounted: function () {
+        if (Object.keys(this.$scopedSlots).length)
+            throw new Error('FmmVueStore is a contentless tag');
+        this.$emit('store', (this.store = new core_1.FmmMapStore(this.values, this.errors)));
+    },
+    // =============================================================================================================================
+    name: 'FmmVueStore',
+    // =============================================================================================================================
+    props: {
+        errors: Object,
+        values: {
+            required: true,
+            type: Object
+        }
+    },
+    // =============================================================================================================================
+    render: function () { return undefined; },
+    // =============================================================================================================================
+    watch: {
+        errors: function () {
+            this.store.update(this.values, this.errors);
+        },
+        values: function () {
+            this.store.update(this.values, this.errors);
+        }
+    }
+});
+// =================================================================================================================================
+//						F M M V U E X
+// =================================================================================================================================
+exports.FmmVuex = vue_1.default.extend({
+    // =============================================================================================================================
+    data: function () {
+        return {
+            $store: undefined,
+            unsubscribeToStore: undefined,
+            store: undefined
+        };
+    },
+    // =============================================================================================================================
+    destroyed: function () {
+        this.unsubscribeToStore();
+    },
+    // =============================================================================================================================
+    mounted: function () {
+        var _this = this;
+        if (Object.keys(this.$scopedSlots).length)
+            throw new Error('FmmVuex is a contentless tag');
+        this.$emit('store', (this.store = new core_1.FmmMapStore(this.$store.state, this.errors)));
+        this.unsubscribeToStore = this.$store.subscribe(function (_, state) { return _this.store.update(state, _this.errors); });
+    },
+    // =============================================================================================================================
+    name: 'FmmVuex',
+    // =============================================================================================================================
+    props: {
+        errors: Object
+    },
+    // =============================================================================================================================
+    render: function () { return undefined; },
+    // =============================================================================================================================
+    watch: {
+        errors: function () {
+            this.store.update(this.$store.state, this.errors);
+        }
+    }
+});
+// =================================================================================================================================
+// =================================================================================================================================
+// =================================================	P R I V A T E	============================================================
+// =================================================================================================================================
+// =================================================================================================================================
+// =================================================================================================================================
+//						G
+// =================================================================================================================================
+var G = {
+    MINIMAPS: new WeakMap(),
+    PANELS: new WeakMap()
+};
