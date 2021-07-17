29860b92c14d0c74ac82f6340016a355b88d09ee
Sparrowhawk <sparrowhawk-ea@hotmail.com>
2021-07-17 17:13:53 +1000

    Vue 3.1.5 update

--- dist/FmmVue.d.ts
+++ dist/FmmVue.d.ts
@@ -33,6 +33,21 @@ export declare const FmmVueMinimap: import("vue").DefineComponent<{
     destructor(): void;
     takeSnapshot(): boolean;
 }, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    aggregateLabels?: unknown;
+    anchor?: unknown;
+    customWidgetIds?: unknown;
+    debounceMsec?: unknown;
+    dynamicLabels?: unknown;
+    framework?: unknown;
+    page?: unknown;
+    panel?: unknown;
+    store?: unknown;
+    title?: unknown;
+    usePanelDetail?: unknown;
+    useWidthToScale?: unknown;
+    verbosity?: unknown;
+    widgetFactories?: unknown;
+} & {
     title: string;
     panel: {
         $: import("vue").ComponentInternalInstance;
@@ -51,7 +66,23 @@ export declare const FmmVueMinimap: import("vue").DefineComponent<{
         $parent: ComponentPublicInstance<{}, {}, {}, {}, {}, {}, {}, {}, false, import("vue").ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}>>;
         $emit: (event: string, ...args: any[]) => void;
         $el: any;
-        $options: import("vue").ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}>;
+        $options: import("vue").ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}> & {
+            beforeCreate?: (() => void) | (() => void)[];
+            created?: (() => void) | (() => void)[];
+            beforeMount?: (() => void) | (() => void)[];
+            mounted?: (() => void) | (() => void)[];
+            beforeUpdate?: (() => void) | (() => void)[];
+            updated?: (() => void) | (() => void)[];
+            activated?: (() => void) | (() => void)[];
+            deactivated?: (() => void) | (() => void)[];
+            beforeDestroy?: (() => void) | (() => void)[];
+            beforeUnmount?: (() => void) | (() => void)[];
+            destroyed?: (() => void) | (() => void)[];
+            unmounted?: (() => void) | (() => void)[];
+            renderTracked?: ((e: import("vue").DebuggerEvent) => void) | ((e: import("vue").DebuggerEvent) => void)[];
+            renderTriggered?: ((e: import("vue").DebuggerEvent) => void) | ((e: import("vue").DebuggerEvent) => void)[];
+            errorCaptured?: ((err: unknown, instance: ComponentPublicInstance<{}, {}, {}, {}, {}, {}, {}, {}, false, import("vue").ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}>>, info: string) => boolean | void) | ((err: unknown, instance: ComponentPublicInstance<{}, {}, {}, {}, {}, {}, {}, {}, false, import("vue").ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}>>, info: string) => boolean | void)[];
+        };
         $forceUpdate: import("vue").ReactiveEffect<any>;
         $nextTick: typeof import("vue").nextTick;
         $watch(source: string | Function, cb: Function, options?: import("vue").WatchOptions<boolean>): import("vue").WatchStopHandle;
@@ -82,6 +113,9 @@ export declare const FmmVuePanel: import("vue").DefineComponent<{
 }, unknown, unknown, {}, {
     destroyDetached(): void;
 }, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    detailParent?: unknown;
+    vertical?: unknown;
+} & {
     vertical: boolean;
 } & {
     detailParent?: HTMLDivElement;
@@ -97,6 +131,9 @@ export declare const FmmVueStore: import("vue").DefineComponent<{
 }, unknown, {
     store: FmmMapStore<FmmMapValues, FmmMapErrors>;
 }, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, "store"[], "store", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    errors?: unknown;
+    values?: unknown;
+} & {
     values: FmmMapValues;
 } & {
     errors?: FmmMapErrors;
@@ -107,6 +144,8 @@ export declare const FmmVuex: import("vue").DefineComponent<{
     $store: any;
     unsubscribeToStore: () => void;
     store: FmmMapStore<FmmMapValues, FmmMapErrors>;
-}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, "store"[], "store", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{} & {
+}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, "store"[], "store", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    errors?: unknown;
+} & {} & {
     errors?: FmmMapErrors;
 }>, {}>;
